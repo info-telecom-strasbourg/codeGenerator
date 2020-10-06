da052c6e0e20b5a783a35592ec55110041646774
ThomasRives <thomas.rives29@gmail.com>
2020-10-06 13:22:53 +0200

    hash map struct

--- C/data_struct/hash-table/hashmap.h
+++ C/data_struct/hash-table/hashmap.h
@@ -7,7 +7,8 @@
 
 typedef struct node
 {
-    unsigned long long key;
+    void               *key;
+	unsigned long long index;
     void               *val;
     struct node        *next;
 } hash_node_t;
@@ -15,18 +16,19 @@ typedef struct node
 typedef struct table
 {
     unsigned long long size;
-    hash_node_t   **list;
+	size_t             node_memsize;
+    hash_node_t        **list;
 } hash_table_t;
 
 /**
  * Cree une hashMap
  */
-hash_table_t *createTable(unsigned long long size);
+hash_table_t *create_table(unsigned long long size, size_t node_memsize);
 
 /**
  * Recupere la position dans la hashmap à partir de la clé
  */
-unsigned long long hashCode(hash_table_t *t, unsigned long long key);
+unsigned long long hash_code_int(hash_table_t *t, unsigned long long key);
 
 /**
  * Insère une valeur dans la hashmap pour une clé donnée
@@ -42,12 +44,12 @@ void* lookup(hash_table_t *t, unsigned long long key);
  * Recherche l'adresse du noeud associée à une clé donnée
  * un noeud est un elément de la hashmap (une hashmap est un tableau de noeuds)
  */
-hash_node_t** lookupNode(hash_table_t *t, unsigned long long key);
+hash_node_t** lookup_node(hash_table_t *t, unsigned long long key);
 
 /**
  * Libère la hashmap
  */
-void freeHashMap(hash_table_t *hMap);
+void free_hash_map(hash_table_t *hMap);
 
 
 #endif //HASH_TABLE_HASHMAP_H
